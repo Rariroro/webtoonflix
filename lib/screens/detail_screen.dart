0c421ecc5725c31dc81be43c20d01c3419b558da
Rariroro <eunga0110@gmail.com>
2024-02-02 18:09:24 +0900

    .

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:webtoonflix/models/webtoon_detail_model.dart';
+import 'package:webtoonflix/models/webtoon_episode_model.dart';
+import 'package:webtoonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
   const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +38,7 @@ class DetailScreen extends StatelessWidget {
         shadowColor: Colors.black,
         elevation: 2,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
         ),
         backgroundColor: Colors.white,
@@ -33,7 +53,7 @@ class DetailScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   width: 250,
                   clipBehavior: Clip.hardEdge,
@@ -48,7 +68,7 @@ class DetailScreen extends StatelessWidget {
                     ],
                   ),
                   child: Image.network(
-                    thumb,
+                    widget.thumb,
                     headers: const {
                       'Referer': 'https://comic.naver.com',
                     },
@@ -57,6 +77,46 @@ class DetailScreen extends StatelessWidget {
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return const Text("...");
+            },
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          FutureBuilder(
+            future: episodes,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {}
+              return Container();
+            },
+          )
         ],
       ),
     );
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoonflix/models/webtoon_detail_model.dart';
 import 'package:webtoonflix/models/webtoon_model.dart';
+import 'package:webtoonflix/models/webtoon_episode_model.dart';
 
 class ApiService {
   //state 가 없는 클래스는 static으로 만든다.
@@ -13,7 +15,6 @@ class ApiService {
     List<WebtoonModel> webtoonInstances = [];
 
     final url = Uri.parse('$baseUrl/$today');
-
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -25,6 +26,35 @@ class ApiService {
 
       //print(response.body);
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    final reponse = await http.get(url);
+    if (reponse.statusCode == 200) {
+      final webtoon = jsonDecode(reponse.body);
+
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodeInstances = [];
+
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final reponse = await http.get(url);
+
+    if (reponse.statusCode == 200) {
+      final episodes = jsonDecode(reponse.body);
+
+      for (var episode in episodes) {
+        episodeInstances.add(WebtoonEpisodeModel.fromjson(episode));
+      }
+      return episodeInstances;
     }
     throw Error();
   }
